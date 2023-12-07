677047c233d4d14ec98f1dda55e4d8684d23f5c6
BalascanGabriel <balascangabriel@email.com>
2023-12-07 15:27:42 +0200

    Revert "More components and concepts"
    This reverts commit 8e9df1ed7a70be8c388e1d1b866c7a257ca3a713.

--- src/components/CoreConcept/CoreConcept.jsx
+++ src/components/CoreConcept/CoreConcept.jsx
@@ -1,23 +1,11 @@
 import './CoreConcept.css'
 
-// export default function CoreConcept(props) {
-//   return (
-//     <li>
-//       <img src={props.image} alt={props.title} />
-//       <h3>{props.title}</h3>
-//       <p>{props.description}</p>
-//     </li>
-//   )
-// }
-
-//sau alta structura
-//Folosind destructurare se iau din data.js fix fieldurile care sunt necesare intre {}
-export default function CoreConcept({ image, title, description }) {
-  return (
-    <li>
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  )
-}
+export default function CoreConcept(props) {
+    return (
+      <li>
+        <img src={props.image} alt={props.title} />
+        <h3>{props.title}</h3>
+        <p>{props.description}</p>
+      </li>
+    )
+  }
