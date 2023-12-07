eaafa44b8ebc1e3b3619516a24dbe905cb3342ab
BalascanGabriel <balascangabriel@email.com>
2023-12-07 15:27:53 +0200

    Revert "Revert "More components and concepts""
    This reverts commit 677047c233d4d14ec98f1dda55e4d8684d23f5c6.

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css'
+
+function Footer() {
+  return (
+    <footer>
+      <p>© 2023 Task App. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
