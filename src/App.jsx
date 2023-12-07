5d2e60e7851a66fcde683ca1db8834f8fec0bee1
BalascanGabriel <balascangabriel@email.com>
2023-12-07 15:28:39 +0200

    Revert "Merge branch 'main' of https://github.com/BalascanGabriel/TaskApp-React-Frontend"
    This reverts commit ae7d1d1564c88aba80d5d4a8e5bafd2c8076657d, reversing
    changes made to eaafa44b8ebc1e3b3619516a24dbe905cb3342ab.

--- src/App.jsx
+++ src/App.jsx
@@ -2,46 +2,50 @@
 
 import { CORE_CONCEPTS } from './data'
 import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer';
 import CoreConcept from './components/CoreConcept/CoreConcept';
-import TabButton from './components/TabButton';
+import TabButton from './components/TabButton/TabButton';
 
 function App() {
+
+  let tabContent = 'Please click a button'
+
+  function handleSelect(selectedButton) {
+    tabContent = selectedButton
+    console.log(selectedButton)
+  }
+
+
   return (
     <div>
       <Header />
 
       <main>
+        <h2>Time to get started!</h2>
+
         <section id="core-concepts">
-          <h2>Core concepts</h2>
+          <h2>Core concepts : </h2>
           <ul>
             <CoreConcept {...CORE_CONCEPTS[0]} />
             <CoreConcept {...CORE_CONCEPTS[1]} />
-            <CoreConcept
-              title={CORE_CONCEPTS[2].title}
-              description={CORE_CONCEPTS[2].description}
-              image={CORE_CONCEPTS[2].image}
-            />
-            <CoreConcept
-              title={CORE_CONCEPTS[3].title}
-              description={CORE_CONCEPTS[3].description}
-              image={CORE_CONCEPTS[3].image}
-            />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
         </section>
 
         <section id="examples">
-          <h2>Examples</h2>
-
+          <h2>Examples : </h2>
           <menu>
-            <TabButton>Components</TabButton>
-            <TabButton>JSX</TabButton>
-            <TabButton>Props</TabButton>
-            <TabButton>Components</TabButton>
+            <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
+            <TabButton onSelect={() => handleSelect('JSX')}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-
+          {tabContent}
         </section>
-        <h2>Time to get started!</h2>
       </main>
+
+      <Footer />
     </div>
   );
 }
